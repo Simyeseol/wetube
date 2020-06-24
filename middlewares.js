c925fb699d063d8a2684bc18b40d6379d806cfd7
Simyeseol <43331033+Simyeseol@users.noreply.github.com>
2020-06-24 23:53:14 +0900

    ~#2.25
    -

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,12 @@
 import routes from "./routes";
+import { reset } from "nodemon";
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "weTube";
     res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated: true,
+        id: 1
+    }
     next();
 };
